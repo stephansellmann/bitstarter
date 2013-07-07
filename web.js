ee83b669ab537bfa73d2fcf2d68ef892388582e7
Stephan Sellmann <Stephan.Sellmann@gmail.com>
2013-07-07 20:07:55 +0000

    update web.js serve index.html fix 1

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var out = fs.readFileSync(index.html,'utf8')
+	var out = fs.readFileSync(./index.html,'utf8')
 	response.send(out);
   //response.send('Hello World 2!');
 });
