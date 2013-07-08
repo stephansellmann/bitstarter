22925521459454efd11a6b63b813fff22b83da97
Stephan Sellmann <Stephan.Sellmann@gmail.com>
2013-07-08 06:47:10 +0000

    update web.js fix5

--- web.js
+++ web.js
@@ -3,9 +3,19 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	//var out = fs.readFileSync(./index.html,'utf8')
+	//var out = fs.readFileSync(./index.html,'utf-8')
 	//response.end(out.toString());
-	response.send('Hello World 2!');
+	/*
+	fs.readFile('./index.html', function (err, data) {
+		if (err) throw err;
+		console.log(data);
+		response.end(data);
+	});
+	*/
+	var html = fs.readFileSync('index.html').toString();
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end(html);
+	//response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
